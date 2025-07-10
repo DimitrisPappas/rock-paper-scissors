3f74a935850216a085118e55e2d240aa6b452c00
DimitrisPappas <dimitris.pappas@infoteam-software.gr>
2025-07-10 11:30:52 +0300

    feat: add player and choice

--- src/main/java/org/dspappas/Game.java
+++ src/main/java/org/dspappas/Game.java
@@ -2,17 +2,5 @@ package org.dspappas;
 
 public class Game {
 
-    Player playerOne = new Player(
-            "Player 1",
-            Choice.PAPER
-    );
-
-    Player playerTwo = new Player(
-            "Player 2"
-    );
-
-    public void pressStart() {
-
-    }
 
 }
--- src/main/java/org/dspappas/Main.java
+++ src/main/java/org/dspappas/Main.java
@@ -3,6 +3,8 @@ package org.dspappas;
 public class Main {
     public static void main(String[] args) {
 
-
+        // Start Game
+        Game game = new Game();
+//        game.pressStart();
     }
 }
