ed649f1f2d39f5675af52baacbda867e74e6427c
DimitrisPappas <dimitris.pappas@infoteam-software.gr>
2025-07-11 16:58:52 +0300

    feat: add game

--- src/main/java/org/dspappas/Game.java
+++ src/main/java/org/dspappas/Game.java
@@ -8,18 +8,20 @@ import static java.lang.Math.*;
 public class Game {
 
     public Player playerOne = new Player(
-            "Player 1",
+            "Player A",
             Choice.PAPER
     );
 
     public Player playerTwo = new Player(
-            "Player 2"
+            "Player B"
     );
 
     public void pressStart() {
-        System.out.println("Press Start");
-        playerTwo.hand = randomChoice();
-
+        for (int i = 0; i < 100; i++) {
+            playerTwo.hand = randomChoice();
+            rockPaperScissorsRound(playerTwo.hand);
+        }
+        displayOutput();
     }
 
     private Choice randomChoice() {
@@ -33,5 +35,34 @@ public class Game {
             default -> result;
         };
         return result;
+    }
+
+    private void rockPaperScissorsRound(Choice handTwo) {
+        if (handTwo == Choice.ROCK) {
+            playerOne.score += 1;
+        }
+        if (handTwo == Choice.SCISSORS) {
+            playerTwo.score += 1;
+        }
+    }
+
+    private void displayOutput() {
+        int tiedRounds = 100 - playerOne.score - playerTwo.score;
+        System.out.printf("%s wins %d of 100 games\n", playerOne.name, playerOne.score);
+        System.out.printf("%s wins %d of 100 games\n", playerTwo.name, playerTwo.score);
+        System.out.printf("Tie: %d of 100 games\n", tiedRounds);
+        System.out.println(displayWinner());
+    }
+
+    private String displayWinner() {
+        String winner = "Winner: ";
+        if (playerOne.score == playerTwo.score) {
+            winner = "TIE";
+        } else if (playerOne.score > playerTwo.score) {
+            winner += playerOne.name;
+        } else {
+            winner += playerTwo.name;
+        }
+        return winner;
     }
 }
