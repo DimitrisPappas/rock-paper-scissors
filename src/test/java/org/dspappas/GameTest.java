f0f15fdda865dd2a3984a7b3d74a32a44cc43724
DimitrisPappas <dimitris.pappas@infoteam-software.gr>
2025-07-14 15:44:42 +0300

    fix: unit tests

--- src/test/java/org/dspappas/GameTest.java
+++ src/test/java/org/dspappas/GameTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class GameServiceTest {
+class GameTest {
 
     @InjectMocks
     private Game sut;
